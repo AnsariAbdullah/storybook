b7d118e62a4e7814cbe959f13c0b01ea08b05765
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-03-07 18:18:37 +0530

    add story within story

---
+++ src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Large } from '../Input/Input.stories';
+import { Primary } from '../Button/Button.stories';
+
+
+export default {
+	title: 'Form/Subsciption'
+}
+
+export const Subsciption = () => (
+	<>
+		<Large />
+		<Primary />
+	</>
+)
